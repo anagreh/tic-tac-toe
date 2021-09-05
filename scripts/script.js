89589cbe9ba78f488dcf831105aa1eee60db8214
Emam Al Mounqer -win <emam.anagreh@gmail.com>
2021-09-05 21:51:16 +0300

    add pubsub and some animation

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Player = (id,name,symbol,color,imgSrc)=>{
+const PlayerFactories = (id,name,symbol,color,imgSrc)=>{
 
     const getId= ()=> id;                //WRONG because I can still edit it from outside the scoop
     const getName = () => name;
@@ -13,15 +13,70 @@ const Player = (id,name,symbol,color,imgSrc)=>{
 };
 
 
+const Players = (function (){
+
+    let players = [];
+    
+
+
+    //initialize
+    const player1 = PlayerFactories(1,'player1','x','red',"images/x-mark-1.svg");
+    const player2 = PlayerFactories(2,'player2','o','blue',"images/circle-2.svg");
+    players = [player1, player2]
+    
+    
+    
+    //catch DOM 
+    const playersDom = document.getElementById('players');
+    const addPlayerForm = playersDom.querySelector('#add-player')
+    const playerName = addPlayerForm.querySelector('#player-name')
+    const playerSubmitButton = addPlayerForm.querySelector('#player-submit-button')
+    const output = playersDom.querySelector('#output')
+    
+
+
+    //bind event
+    addPlayerForm.addEventListener('submit',e => addPlayer(e));
+
+
+    //render
+    players.forEach(player => {
+        const div= document.createElement('div');
+        div.textContent = player.getName() +' : '+ player.getSymbol();
+        output.appendChild(div);
+    });
+
+    //---------------------------------
+    function addPlayer(e){
+
+        e.preventDefault();
+
+        
+
+    }
+        
+
+
+})();
+
+
 const play = (function(){
 
     let players = [];
-    const player1 = Player(1,'player1','x','red',"images/x-mark-1.svg");
-    const player2 = Player(2,'player2','o','blue',"images/circle-2.svg");
+
+    const player1 = PlayerFactories(1,'player1','x','red',"images/x-mark-1.svg");
+    const player2 = PlayerFactories(2,'player2','o','blue',"images/circle-2.svg");
     players = [player1, player2]
 
     let playersTurn = players[0];
+    
+    //event----------------------------
+    events.on('turnEnd', changePlayerTurn)
+    
 
+
+
+    //---------------------------------
     function changePlayerTurn(){
 
         const oldPlayerIndex = players.indexOf(playersTurn)
@@ -30,68 +85,6 @@ const play = (function(){
 
     }
 
-    //-------------
-    function gameResult(gameBoard , isGmaeBoardFull){
-
-        if (checkRowColDia(gameBoard)){
-            // boardTable.removeEventListener('click', updatGameBoardArray);
-            console.log('win: ',playersTurn.getName()); 
-            return true;
-        }else if(isGmaeBoardFull){
-            console.log('draw'); 
-            return true;
-        }
-
-
-
-        
-        function checkRowColDia(gameBoard){
-                
-
-            for (let r = 0; r < gameBoard.length; r++) {
-                for (let c = 1; c < gameBoard.length; c++) {
-                    // console.log('[',0 ,',',c-1 , '] =',gameBoard[0][c-1],'\n'  ,'[',0 ,',',c , '] =',gameBoard[0][c]);
-        
-                    if(gameBoard[r][c-1] === 0  || !(gameBoard[r][c] === gameBoard[r][c-1]) )break ;
-                    else if (c === gameBoard.length-1 ) {
-                        return playersTurn;
-                    }
-        
-                }
-            }
-            for (let c = 0; c < gameBoard.length; c++) {
-                for (let r = 1; r < gameBoard.length; r++) {
-                    // console.log('[',0 ,',',c-1 , '] =',gameBoard[0][c-1],'\n'  ,'[',0 ,',',c , '] =',gameBoard[0][c]);
-        
-                    if(gameBoard[r-1][c] === 0  || !(gameBoard[r][c] === gameBoard[r-1][c]) )break ;
-                    else if (r === gameBoard.length-1 ) {
-                        return playersTurn;
-                    }
-        
-                }
-            }
-
-            for (let rc = 1; rc < gameBoard.length; rc++) {
-                if(gameBoard[rc-1][rc-1] === 0  || !(gameBoard[rc][rc] === gameBoard[rc-1][rc-1]) )break ;
-                else if (rc === gameBoard.length-1 ) {
-                    return playersTurn;
-                }
-                
-            }
-            for (let c = 1; c < gameBoard.length; c++) {
-                if(gameBoard[gameBoard.length-c][c-1] === 0  || !(gameBoard[gameBoard.length-1-c][c] === gameBoard[gameBoard.length-c][c-1]) )break ;
-                else if (c === gameBoard.length-1 ) {
-                    return playersTurn;
-
-                }
-                
-            }
-
-        }
-        
-    }
-
-
 
     const getPlayersTurn= ()=>{ return playersTurn};
 
@@ -99,72 +92,78 @@ const play = (function(){
 
     return {
         getPlayersTurn,
-        changePlayerTurn,
-        gameResult
+        changePlayerTurn
     }
 
 })();
 
 
-//=====================================================================================================
+
+
+
+//=================================================================================================
 const gameBoard = (function (){
     'use strict';
 
     let gameBoard = [];
+    let boardDim;
+
     const status = {     
-        isGmaeBoardFull : false,
+        isGameBoardFull : false,
         numberOfSpaceTaken :0,
         resit: function(){
-            this.isGmaeBoardFull = false;
+            this.isGameBoardFull = false;
             this.numberOfSpaceTaken = 0;
         }
     }
-    let boardDim = 3;
-    document.documentElement.style.setProperty('--dim', boardDim)
+    
 
-    //initilize
-    initiateGirdToZero();
+    //initialize------------------------
+    boardDim = 3;
+    initiateGridToZero();
 
 
-    // catch dom
+    // catch dom-----------------------
     const boardTable = document.querySelector(".table");
     const restButton = document.querySelector(".button");
-    const slider = document.getElementById("myRange");
-    slider.value=boardDim;
 
-    //bind event
-    boardTable.addEventListener('click', updatGameBoardArray);
+
+    //bind event------------------------
+    boardTable.addEventListener('click', updateGameBoardArray);
     restButton.addEventListener('click', resetGameBoard); 
-    slider.addEventListener('input', getSlider); 
+    events.on('sliderChanged', updateBoardDim)
+    events.on('gameEnded', whenGameEnd )
+    
 
-
-    //render
+    //render-----------------------------
+    document.documentElement.style.setProperty('--dim', boardDim)
     doForAllGridElements(addDivForEachGridElement);
     function updateGameBoardUI(clickedDiv){
-        // clickedDiv.textContent = play.getPlayersTurn().getSymbol();
-        // clickedDiv.style.backgroundColor = play.getPlayersTurn().getColor();
+
         addImage(clickedDiv);
-        addToNumberOfSpaceTaken();
-        if(play.gameResult(gameBoard , status.isGmaeBoardFull)) {
-            boardTable.removeEventListener('click', updatGameBoardArray);
-        };
-        play.changePlayerTurn();
+        UpdateNumberOfSpaceTaken();
+        resultCheck();
+        events.emit('turnEnd')
     }
+
+    //reset--------------------------------
     function resetGameBoard(){
     
         gameBoard =[]
         document.documentElement.style.setProperty('--dim', boardDim)
-        initiateGirdToZero();
+        initiateGridToZero();
         boardTable.replaceChildren();
         doForAllGridElements(addDivForEachGridElement);
-        boardTable.removeEventListener('click', updatGameBoardArray);
-        boardTable.addEventListener('click', updatGameBoardArray);
+        boardTable.removeEventListener('click', updateGameBoardArray);
+        boardTable.addEventListener('click', updateGameBoardArray);
         status.resit();
         
     }
 
-    //--------------
-    function updatGameBoardArray(e){
+
+
+    //--------------------------------------
+    function updateGameBoardArray(e){
         const clickedDiv = e.target;
         if (clickedDiv.parentElement === boardTable){
 
@@ -180,7 +179,7 @@ const gameBoard = (function (){
         } 
     }
 
-    function initiateGirdToZero(){
+    function initiateGridToZero(){
 
         for(let r=0; r < boardDim; r++){
             gameBoard[r] = [];
@@ -211,82 +210,94 @@ const gameBoard = (function (){
 
     function getRCFromID(id){
 
-    const r = Number(id.substring(id.indexOf("r")+1, id.indexOf("-"))) ;
-    const c = Number(id.substring(id.indexOf("c")+1, id.lenght) );
-    return [r,c]
+        const r = Number(id.substring(id.indexOf("r")+1, id.indexOf("-"))) ;
+        const c = Number(id.substring(id.indexOf("c")+1) );
+        return [r,c]
 
     }
-
-    function addToNumberOfSpaceTaken(){
+    
+    function UpdateNumberOfSpaceTaken(){
         
-        if (status.numberOfSpaceTaken < (boardDim*boardDim) ) {  //first if statment not nedded
+        if (status.numberOfSpaceTaken < (boardDim*boardDim) ) {  
             status.numberOfSpaceTaken ++;
-            if (status.numberOfSpaceTaken == (boardDim*boardDim) )status.isGmaeBoardFull = true;
+            if (status.numberOfSpaceTaken == (boardDim*boardDim) )status.isGameBoardFull = true;
         }
 
     }
 
-    // function gameResult(){
-
-    //     if (checkRowColDia()){
-    //         boardTable.removeEventListener('click', updatGameBoardArray);
-    //         console.log('win: ',play.getPlayersTurn().getName()); 
-    //     }else if(status.isGmaeBoardFull){
-    //         console.log('draw'); 
-    //     }
-    // }
-
-    // function checkRowColDia(){
-            
-
-    //     for (let r = 0; r < gameBoard.length; r++) {
-    //         for (let c = 1; c < gameBoard.length; c++) {
-    //             // console.log('[',0 ,',',c-1 , '] =',gameBoard[0][c-1],'\n'  ,'[',0 ,',',c , '] =',gameBoard[0][c]);
-    
-    //             if(gameBoard[r][c-1] === 0  || !(gameBoard[r][c] === gameBoard[r][c-1]) )break ;
-    //             else if (c === gameBoard.length-1 ) {
-    //                 return play.getPlayersTurn();
-    //             }
-      
-    //         }
-    //     }
-    //     for (let c = 0; c < gameBoard.length; c++) {
-    //         for (let r = 1; r < gameBoard.length; r++) {
-    //             // console.log('[',0 ,',',c-1 , '] =',gameBoard[0][c-1],'\n'  ,'[',0 ,',',c , '] =',gameBoard[0][c]);
-    
-    //             if(gameBoard[r-1][c] === 0  || !(gameBoard[r][c] === gameBoard[r-1][c]) )break ;
-    //             else if (r === gameBoard.length-1 ) {
-    //                 return play.getPlayersTurn();
-    //             }
-      
-    //         }
-    //     }
-
-    //     for (let rc = 1; rc < gameBoard.length; rc++) {
-    //         if(gameBoard[rc-1][rc-1] === 0  || !(gameBoard[rc][rc] === gameBoard[rc-1][rc-1]) )break ;
-    //         else if (rc === gameBoard.length-1 ) {
-    //             return play.getPlayersTurn();
-    //         }
-            
-    //     }
-    //     for (let c = 1; c < gameBoard.length; c++) {
-    //         if(gameBoard[gameBoard.length-c][c-1] === 0  || !(gameBoard[gameBoard.length-1-c][c] === gameBoard[gameBoard.length-c][c-1]) )break ;
-    //         else if (c === gameBoard.length-1 ) {
-    //             return play.getPlayersTurn();
-
-    //         }
-            
-    //     }
-
-    // }
-
-    
-    function getSlider() {
-        console.log( this.value);
-        boardDim= this.value;
+    function updateBoardDim(slider) {
+        boardDim= slider.value;
         resetGameBoard();
-
     } 
+
+    function resultCheck(){
+
+        const isThereWinner = Boolean( checkRowColDia() );
+
+        if (isThereWinner || status.isGameBoardFull) {
+            let result;
+            
+            if (isThereWinner){
+                console.log('win: ',play.getPlayersTurn().getName()); 
+                result = play.getPlayersTurn().getName();
+                events.emit('winGame' , play.getPlayersTurn())
+                
+            }else if(status.isGameBoardFull){
+                console.log('draw');
+                result = 'draw'
+                events.emit('draw')
+                
+            }
+            events.emit('gameEnded' , result)
+        }
+
+
+        
+        function checkRowColDia(){
+                
+
+            for (let r = 0; r < gameBoard.length; r++) {
+                for (let c = 1; c < gameBoard.length; c++) {
+                    // console.log('[',0 ,',',c-1 , '] =',gameBoard[0][c-1],'\n'  ,'[',0 ,',',c , '] =',gameBoard[0][c]);
+        
+                    if(gameBoard[r][c-1] === 0  || !(gameBoard[r][c] === gameBoard[r][c-1]) )break ;
+                    else if (c === gameBoard.length-1 ) {
+                        return play.getPlayersTurn().getName();
+                    }
+        
+                }
+            }
+            for (let c = 0; c < gameBoard.length; c++) {
+                for (let r = 1; r < gameBoard.length; r++) {
+                    // console.log('[',0 ,',',c-1 , '] =',gameBoard[0][c-1],'\n'  ,'[',0 ,',',c , '] =',gameBoard[0][c]);
+        
+                    if(gameBoard[r-1][c] === 0  || !(gameBoard[r][c] === gameBoard[r-1][c]) )break ;
+                    else if (r === gameBoard.length-1 ) {
+                        return play.getPlayersTurn().getName();
+                    }
+        
+                }
+            }
+
+            for (let rc = 1; rc < gameBoard.length; rc++) {
+                if(gameBoard[rc-1][rc-1] === 0  || !(gameBoard[rc][rc] === gameBoard[rc-1][rc-1]) )break ;
+                else if (rc === gameBoard.length-1 ) {
+                    return play.getPlayersTurn().getName();
+                }
+                
+            }
+            for (let c = 1; c < gameBoard.length; c++) {
+                if(gameBoard[gameBoard.length-c][c-1] === 0  || !(gameBoard[gameBoard.length-1-c][c] === gameBoard[gameBoard.length-c][c-1]) )break ;
+                else if (c === gameBoard.length-1 ) {
+                    return play.getPlayersTurn().getName();
+
+                }
+                
+            }
+
+        }
+        
+    }
 
     function addImage(clickedDiv){
         const img = document.createElement("img");
@@ -295,21 +306,50 @@ const gameBoard = (function (){
 
     }
 
+    function whenGameEnd(result){
+        boardTable.removeEventListener('click', updateGameBoardArray)
 
-    const getGmaeBoard = ()=>console.table(gameBoard);
-    const getStatus = () => console.table(status);
-    
-    return{
-        getGmaeBoard,
-        getStatus
+        const template = document.createElement('div')
+        template.classList.add("result-container")
+        template.textContent = (result ==='draw' ? 'DRAW' : "winner : " + result);
+        boardTable.appendChild(template);
+
     }
+
+
+    // const getGameBoard = ()=>console.table(gameBoard);
+    // const getStatus = () => console.table(status);
+
+
+    // return{
+    //     getGameBoard,
+    //     getStatus
+    // }
 
 
 })();
 
 
 
-// let div = document.querySelector("div");
-// div.textContent
+//slider===========================================================================================
+(function(){
+
+    // catch DOM
+    const slider = document.getElementById("myRange");
+
+    //initialize
+    slider.value=3;
+
+    //event
+    slider.addEventListener('input', () => events.emit('sliderChanged' , slider) ); 
+
+
+})();
+
+
+
+
+
+
 
 
